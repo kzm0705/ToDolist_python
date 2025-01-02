2d8384b8dde21699dda5c3e1c9f26ca3a1a58166
kzm0705 <tyoityotyoi@gmail.com>
2025-01-03 08:13:28 +0900

    status

--- sampleToDo.py
+++ sampleToDo.py
@@ -5,15 +5,19 @@ sg.theme('DarkBrown1')
 
 MAX_TODO = 10  #最大で10個
 
-todo_list = [[sg.Checkbox("",key=f'-ch{i+1}-'), sg.Text('',key=f'-todolist{i+1}-'),] for i in range(MAX_TODO)]
+STATUS = ['未完了','進行中','完了']
+
+todo_list = [[sg.Checkbox("",key=f'-ch{i+1}-',size=(1,1)), sg.Text('',key=f'-todolist{i+1}-',size=(30,1)),
+              sg.OptionMenu(STATUS, default_value='未完了',size=(6,1),key=f'-status{i+1}-')] for i in range(MAX_TODO)]
 
 lay_1 = [[sg.Text('just do it !')],
          [sg.Button('追加'),sg.Input(key='-input-', size=(30,1))],
           ]
 
-lay_2 = [[sg.Text('やりたいことリスト'),sg.Button('削除',key='-delete-')],
+lay_2 = [[sg.Text('やりたいことリスト',size=(37,1)),sg.Text('ステータス',),sg.Button('削除',key='-delete-',pad=(15,1))],
          [sg.Column(todo_list, key='-todo list-'),],
          ]
+
 layout = [[sg.Frame('group1',lay_1, vertical_alignment='top'),
           sg.Frame('group2', size=(500,500), key='-list-', layout=lay_2)],
            ]
@@ -31,13 +35,14 @@ while True:
         for i in range(MAX_TODO):
             if window[f'-todolist{i+1}-'].get() == '':
                 window[f'-todolist{i+1}-'].update(do)
+                window['-input-'].update('')
                 break
     if event == '-delete-':
         for i in range(MAX_TODO):
             if window[f'-ch{i+1}-'].get():
                 window[f'-todolist{i+1}-'].update('')
                 window[f'-ch{i+1}-'].update(False)
-
+                window[f'-status{i+1}-'].update('未完了')
 
 
 window.close()
